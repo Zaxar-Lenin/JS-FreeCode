5b526ed2ac65b08503ac43a6ee046d21a83acad5
Zahar Samohval <zaxar137samohval44@gmail.com>
2022-01-16 13:40:40 +0300

    first commit

---
+++ js10.js
@@ -0,0 +1,10 @@
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val >= 25 && val <= 50) {
+        return "Yes";
+    }
+    return "No";
+  }
+  
+  testLogicalAnd(10);
